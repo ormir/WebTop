0bd848a08e4511d5283efd92fdf96cbade33bb48
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-04-11 14:42:41 +0200

    new file upload.php

--- includes/gallery.php
+++ includes/gallery.php
@@ -1,6 +1,6 @@
-<div id='gallery'>
+<div id='gallery' >
 	<div id='gallery-image-upload'>
-		<form>
+		<form action="upload.php" method="post" enctype="multipart/form-data">
 			<input type="text" name="name" id="name">
 			<input type="file" name="file_upload" id="file_upload">
 			<input type="submit" name="submit" value="Submit Content">
