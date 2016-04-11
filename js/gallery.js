10ced1debf3baa93fe01e15f76a60fff17d474b9
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-04-11 13:53:43 +0200

    Deleted older scripts and the gallery.js function

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,5 @@
 $(function() {
-    $('#file_upload').fileupload({
-        dataType: 'json',
-        done: function (e, data) {
-            $.each(data.result.files, function (index, file) {
-                $('<p/>').text(file.name).appendTo(document.body);
-            });
-        }
-    });
+   
 });
 
 
