843fb50e346d5749182c34aca6ce280f4b69d79c
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-04-11 13:50:33 +0200

    comiiit

--- includes/head.php
+++ includes/head.php
@@ -5,3 +5,6 @@
 <link href="http://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
 <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
 <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+<!-- Drop JS -->
+	<script src="http://code.jquery.com/jquery-2.1.1-rc2.min.js" ></script>
+	<script src="http://malsup.github.com/jquery.form.js"></script> 
