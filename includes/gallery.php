731174581c9e52d4cf2611a6b4104e7e76f08fcf
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-04-12 14:20:48 +0200

    gallery update, lorem ipsum deleted, picture showing

--- includes/gallery.php
+++ includes/gallery.php
@@ -11,7 +11,15 @@
 	</form>
 
 	<div id='gallery-images'>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit vel lectus non accumsan. In mauris lacus, congue quis  vel, dapibus iaculis libero. Sed lacus felis, gravida sit amet mi vel, hendrerit consectetur diam. Sed sed pellentesque nisl. Etiam tempus mi massa, vulputate fermentum lorem posuere at. Etiam accumsan tempor nibh ac placerat. In congue ligula quis sem ornare convallis. Etiam maximus est sit amet dolor convallis, eu vehicula nulla cursus. Integer eget dolor ut ante egestas luctus sit amet quis neque. Cras at tempor dolor. Suspendisse at aliquet magna, id suscipit risus. Mauris id volutpat lacus.
+		<?php 
+			$resource = opendir("upload"); //if the folder in our htdocs is called "upload"
+			while (($entry = readdir($resource)) !== FALSE){
+				if($entry != '.' && $entry != '..'){
+					echo "<img src='upload/$entry'>"; 
+					// $file_name.= $entry.'<br>';
+				}
+			}
+		?>
 	</div>
 	<div id="drop-info">
 		<p>DROP</p>
@@ -20,20 +28,21 @@
 
 <!-- Function to display and delete all images -->
 <?php 
-$resource = opendir("upload"); //if the folder in our htdocs is called "upload"
-while (($entry = readdir($resource)) !== FALSE){
-	if($entry != '.' && $entry != '..'){
-		echo "<img src='upload/$entry'>"; 
-		$file_name.= $entry.'<br>';
-	}
-}
-echo '<br>'.$file_name; //file name of the image is shown beneath the image
+// $resource = opendir("upload"); //if the folder in our htdocs is called "upload"
+// while (($entry = readdir($resource)) !== FALSE){
+// 	if($entry != '.' && $entry != '..'){
+// 		echo "<img src='upload/$entry'>"; 
+// 		// $file_name.= $entry.'<br>';
+// 	}
+// }
+// echo '<br>'.$file_name; //file name of the image is shown beneath the image
 	
 
-if(isset($_POST['delete_img'])){		//delete_img is the name of the button to delete the images
-	$img_file = $_POST['filename'];
-	if($img_file){
-		unlink("./upload/$img_file");
-		header("Refresh:0");
-	}
-} 
+// if(isset($_POST['delete_img'])){		//delete_img is the name of the button to delete the images
+// 	$img_file = $_POST['filename'];
+// 	if($img_file){
+// 		unlink("./upload/$img_file");
+// 		header("Refresh:0");
+// 	}
+// } 
+?>
