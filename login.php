cef36366a15805aba632f098a36d3cd42d284fe4
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-04-12 17:43:03 +0200

    committing css and login

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <div id="login">
 	<form role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
 		<img src="images/logo.png">
-		<h2>Login to Webtop</h2>
+		<h2 id="logintext">Login to Webtop</h2>
 		<input type="text" name="username" placeholder="Username" required autofocus></br>
 		<input type="password" name="password" placeholder="Password" required><br>
-		<input type="checkbox" name='remember' value="yes">Remember Me</input><br>
+		<input type="checkbox" name='remember' value="yes"><p id="logintext">Remember Me</p></input><br>
 		<button type="submit" name="login">Login</button>
 	</form>
 </div>
