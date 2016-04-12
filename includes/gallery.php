2ccf3d8b22f04b1b585d0dcf3b64f7c437bec36d
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-04-12 12:06:13 +0200

    gallery stuff
    Function added to open (read) the “upload” folder once the upload
    works. I also added the delete function to remove the unwanted images

--- includes/gallery.php
+++ includes/gallery.php
@@ -7,3 +7,24 @@
 		<p>DROP</p>
 	</div>
 </div>
+
+<!-- Function to display and delete all images -->
+
+<?php 
+$resource = opendir("upload"); //if the folder in our htdocs is called "upload"
+while (($entry = readdir($resource)) !== FALSE){
+	if($entry != '.' && $entry != '..'){
+		echo "<img src='upload/$entry'>"; 
+		$file_name.= $entry.'<br>';
+	}
+}
+echo '<br>'.$file_name; //file name of the image is shown beneath the image
+	
+
+if(isset($_POST['delete_img'])){		//delete_img is the name of the button to delete the images
+	$img_file = $_POST['filename'];
+	if($img_file){
+		unlink("./upload/$img_file");
+		header("Refresh:0");
+	}
+} 
