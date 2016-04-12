c287c0ae50e9a90cc7b2595f7ec8106dcfcf51cc
Ormir G <ormirgj@gmail.com>
2016-04-12 10:41:42 +0200

    hallo

--- includes/gallery.php
+++ includes/gallery.php
@@ -1,7 +1,7 @@
 <div id='gallery'>
 	<div id='gallery-image-upload'></div>
 	<div id='gallery-images'>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit vel lectus non accumsan. In mauris lacus, congue quis tempus vel, dapibus iaculis libero. Sed lacus felis, gravida sit amet mi vel, hendrerit consectetur diam. Sed sed pellentesque nisl. Etiam tempus mi massa, vulputate fermentum lorem posuere at. Etiam accumsan tempor nibh ac placerat. In congue ligula quis sem ornare convallis. Etiam maximus est sit amet dolor convallis, eu vehicula nulla cursus. Integer eget dolor ut ante egestas luctus sit amet quis neque. Cras at tempor dolor. Suspendisse at aliquet magna, id suscipit risus. Mauris id volutpat lacus.
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit vel lectus non accumsan. In mauris lacus, congue quis  vel, dapibus iaculis libero. Sed lacus felis, gravida sit amet mi vel, hendrerit consectetur diam. Sed sed pellentesque nisl. Etiam tempus mi massa, vulputate fermentum lorem posuere at. Etiam accumsan tempor nibh ac placerat. In congue ligula quis sem ornare convallis. Etiam maximus est sit amet dolor convallis, eu vehicula nulla cursus. Integer eget dolor ut ante egestas luctus sit amet quis neque. Cras at tempor dolor. Suspendisse at aliquet magna, id suscipit risus. Mauris id volutpat lacus.
 	</div>
 	<div id="drop-info">
 		<p>DROP</p>
