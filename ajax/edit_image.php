4b43941b00f65f8a52297ea10cfd9b2c7d5117de
Ormir G <ormirgj@gmail.com>
2016-04-15 18:41:40 +0200

    image edip #stuff2

--- ajax/edit_image.php
+++ ajax/edit_image.php
@@ -4,10 +4,11 @@ header('Content-Type: application/json');
 if (isset($_POST['image'])){
 	$imagesrc = $_POST['image']['src'];
 	$edit = $_POST['image']['edit'];
+	$editsuccess = false;
 
 	if (!empty($imagesrc) && !empty($edit)){
 		$imagesrc = "../".$imagesrc;
-		echo "src: ".$imagesrc." edit: ".$edit;
+		// echo "src: ".$imagesrc." edit: ".$edit;
 
 		// Create new image
 		$image = imagecreatefromjpeg($imagesrc);
@@ -15,28 +16,32 @@ if (isset($_POST['image'])){
 		// Edit image
 		switch($edit) {
 			case "mirror":
-				imagefilter($image, IMG_FLIP_BOTH, NULL);
+				$editsuccess = imagefilter($image, IMG_FLIP_BOTH, NULL);
 				break;
 
 			case "greyscale":
-				imagefilter($image, IMG_FILTER_GRAYSCALE);
+				$editsuccess = imagefilter($image, IMG_FILTER_GRAYSCALE);
 				break;
 
 			case "rotate_clockwise":
-				imagerotate($source, 270);
+				$editsuccess = imagerotate($source, 270, NULL);
 				break;
 			case "rotate_anticlockwise":
-				imagerotate($source, 90);
+				$editsuccess = imagerotate($source, 90, NULL);
 				break;
 			default:
 				echo "Edit ".$edit." not recognised";
 		}
 
-		// Save new image
-		imagejpeg($image, $imagesrc);
+		if ($editsuccess) {
+			// Save new image
+			imagejpeg($image, $imagesrc);
 
-		// Send success
-		echo "{success}";
+			// Send success
+			echo "{success}";
+		} else {
+			echo "Edit fail: ".$edit." on ".$imagesrc;
+		}
 
 	} else if (empty($imagesrc)) {
 		echo "missing src";
