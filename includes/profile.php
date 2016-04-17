62f17badfb6bdc034d63db10e5eba31fcb3dc5c8
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-04-17 17:25:51 +0200

    profile echo complete

--- includes/profile.php
+++ includes/profile.php
@@ -7,8 +7,13 @@
 
 	if ($result->num_rows == 1) {
 		$row = $result->fetch_assoc();
-		print_r($row);
-		echo "Vorname: " . $row["firstname"]. "<br>"  . "Nachname: " . $row["lastname"]. "<br>" . "Username: " . $row["username"]. "<br>" . "Email: " . $row["email"] . "<br>";
+		// print_r($row);
+		// echo "Vorname: " . $row["firstname"]. "<br>"  . "Nachname: " . $row["lastname"]. "<br>" . "Username: " . $row["username"]. "<br>" . "Email: " . $row["email"] . "<br>";
+
+		echo "<h3><span class = 'profile_edit' id ='profirstname'>". $row["firstname"]."</span> ". 
+			"<span class = 'profile_edit' id ='prolastname'>".$row["lastname"]."</span></h3>".
+			"<h3 class = 'profile_edit' id ='prousername'>".$row["username"]."</h3><br/>".
+			"<h3 class = 'profile_edit' id ='proemail'>".$row["email"]."</h3><br/>";
 	}
 ?>
 </div>
