df974c93678f6c1c8c5ae76f5c6daf3945ec7cae
Ormir G <ormirgj@gmail.com>
2016-04-17 16:22:37 +0200

    #swag #cool #coolstuff #win

--- includes/profile.php
+++ includes/profile.php
@@ -1,21 +1,14 @@
 <div>
-Hallo
 <?php
-$sql = "SELECT firstname, lastname, username, email FROM user WHERE username = '".$_SESSION['username']."'";
-echo $sql;
+	global $mysqli;
+	$sql = "SELECT firstname, lastname, username, email FROM user WHERE username = '".$_SESSION['username']."'";
+
+	$result = $mysqli->query($sql);
+
+	if ($result->num_rows == 1) {
+		$row = $result->fetch_assoc();
+		print_r($row);
+		echo "Vorname: " . $row["firstname"]. "<br>"  . "Nachname: " . $row["lastname"]. "<br>" . "Username: " . $row["username"]. "<br>" . "Email: " . $row["email"] . "<br>";
+	}
 ?>
 </div>
-
-<?php
-// global $mysqli;
-	// $sql = "SELECT firstname, lastname, username, email FROM user WHERE username = $_SESSION['username']";
-	// echo $sql;
-	// $result = $mysqli->query($sql);
-
-	// if ($result->num_rows == 1) {
-	//     while($row = $result->fetch_assoc()) {
-	//         echo "Vorname: " . $row["firstname"]. "<br>"  . "Nachname: " . $row["lastname"]. "<br>" . "Username: " . $row["username"]. "<br>" . "Email: " . $row["email"] . "<br>";
-	//     }
- //    }
-    
-?> 
