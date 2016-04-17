eda510d03aa57fe0ceedb5a3b058e226520e22b8
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-04-17 15:51:18 +0200

    committing profile FAIL

--- includes/profile.php
+++ includes/profile.php
@@ -1,14 +1,13 @@
 <?php
-global $mysqli;
+// global $mysqli;
+	$sql = "SELECT firstname, lastname, username, email FROM user WHERE username = $_SESSION['username']";
+	echo $sql;
+	// $result = $mysqli->query($sql);
 
-<div id = "profile">
-	$sql = "SELECT firstname, lastname, username, email";
-	$result = $mysqli->query($sql);
-
-	if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "Vorname: " . $row["firstname"]. "<br>"  . "Nachname: " . $row["lastname"]. "<br>" . "Username: " . $row["username"]. "<br>" . "Email: " . $row["email"] . "<br>";
-    mysqli_close($mysqli_);
-
-</div>
+	// if ($result->num_rows == 1) {
+	//     while($row = $result->fetch_assoc()) {
+	//         echo "Vorname: " . $row["firstname"]. "<br>"  . "Nachname: " . $row["lastname"]. "<br>" . "Username: " . $row["username"]. "<br>" . "Email: " . $row["email"] . "<br>";
+	//     }
+ //    }
+    
 ?> 
