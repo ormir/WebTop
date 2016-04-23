4f13b837e5c0c0869da39ae6d43be255c49e58e6
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-04-23 12:05:33 +0200

    blah

--- includes/forgotten.php
+++ includes/forgotten.php
@@ -1,8 +1,8 @@
 <div id="recovery">
-	<img id="login-icon" src="images/logo.png">
-	<h2 id="logintext">Password Recovery</h2>
-	<form role="form" method="POST" action="">
-		<input type="text" name="username" placeholder="Username" autofocus required>
+	<form role="form" method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+		<img id="login-icon" src="images/logo.png">
+		<h2 id="logintext">Password Recovery</h2>
+		<input type="text" name="username" placeholder="Username" autofocus required><br />
 		<button type="submit" name="recover">Send Password</button>
 	</form>
 
