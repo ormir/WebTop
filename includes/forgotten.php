0b4ef4d3859faf0ae26bfb3adbfa1bfde94e2b4c
Ormir G <ormirgj@gmail.com>
2016-04-23 12:03:56 +0200

    reset pass #stuff

--- includes/forgotten.php
+++ includes/forgotten.php
@@ -1,9 +1,8 @@
-<div id="recovery">
-	<img id="login-icon" src="images/logo.png">
-	<h2 id="logintext">Password Recovery</h2>
-	<form role="form" method="POST" action="">
-		<input type="text" name="username" placeholder="Username" autofocus required>
-		<button type="submit" name="recover">Send Password</button>
+<div id="login">
+	<form role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+		<img id="login-icon" src="images/logo.png">
+		<h2 id="logintext">Password Recovery</h2>
+		<input type="text" name="username" placeholder="Username" required autofocus></br>
+		<button type="submit" name="login">Reset</button><br/>
 	</form>
-
 </div>
