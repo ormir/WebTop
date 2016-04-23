5d560336324b3cab0a2e4ecc7c70d0efaa4e0d61
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-04-23 12:00:52 +0200

    commit

--- includes/forgotten.php
+++ includes/forgotten.php
@@ -1,2 +1,9 @@
-
+<div id="recovery">
 	<img id="login-icon" src="images/logo.png">
+	<h2 id="logintext">Password Recovery</h2>
+	<form role="form" method="POST" action="">
+		<input type="text" name="username" placeholder="Username" autofocus required>
+		<button type="submit" name="recover">Send Password</button>
+	</form>
+
+</div>
