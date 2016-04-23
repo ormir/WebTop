88fb5b542c95d3a67c44f61760afb92492e0cea8
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-04-23 15:00:12 +0200

    saveposition changes?

--- ajax/save_position.php
+++ ajax/save_position.php
@@ -1,10 +1,10 @@
 <?php 
-session_start();
-header('Content-Type: application/json');
+// session_start();
+// header('Content-Type: application/json');
 
-if (isset($_POST['element'])){
-	$element = $_POST['element'];
-	$_SESSION['elements'][$element['id']] = ['top' => $element['top'], 'left' => $element['left']];
-	echo json_encode($_SESSION['elements']);
-}
-?>
+// if (isset($_POST['element'])){
+// 	$element = $_POST['element'];
+// 	$_SESSION['elements'][$element['id']] = ['top' => $element['top'], 'left' => $element['left']];
+// 	echo json_encode($_SESSION['elements']);
+// }
+?> 
