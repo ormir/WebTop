8a8c095b06cfbf719e53d8df0f766b3897507892
Ormir G <ormirgj@gmail.com>
2016-05-09 17:41:56 +0200

    feed update

--- ajax/feed.php
+++ ajax/feed.php
@@ -4,22 +4,23 @@ header('Content-Type: application/json');
 include "../includes/db.php";
 
 if (isset($_POST['feed_read'])){
-	// echo '{"succes" : 1, "message" : "Read feed from: '.$_POST['feed_read'].'"}';
 	$myXML = simplexml_load_file($_POST['feed_read']);
+	if ($myXML) {
+		$responce = array();
+		$responce['success'] = 1;
+		foreach($myXML->entry as $myEntry){
+			$responce['feed']["f".$myEntry->id] = ['id' => $myEntry->id->__toString(), 
+											'title' => $myEntry->title[0]->__toString(), 
+											'link' => $myEntry->id->__toString(),
+											'description' => $myEntry->summary->__toString(),
+											'date' => $myEntry->published->__toString()];
+		}
 
-	$responce = array();
-	$responce['success'] = 1;
-	foreach($myXML->entry as $myEntry){
-		$responce['feed']["f".$myEntry->id] = ['id' => $myEntry->id->__toString(), 
-										'title' => $myEntry->title[0]->__toString(), 
-										'link' => $myEntry->id->__toString(),
-										'description' => $myEntry->summary->__toString(),
-										'date' => $myEntry->published->__toString()];
+		echo json_encode($responce);
+	} else {
+		echo '{"succes" : 0, "message" : "Could not read from feed"}';
 	}
-
-	echo json_encode($responce);
 } else {
 	echo '{"succes" : 0, "message" : "No feed found"}';
 }
-
 ?>
