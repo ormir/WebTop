afe7fff20617d2a3f12053a372f12d8740ee4795
Ormir G <ormirgj@gmail.com>
2016-05-19 16:03:37 +0200

    weather ajax

--- ajax/weather_data.php
+++ ajax/weather_data.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 
-if (isset($_POST['cityZIP'])){
+if (isset($_POST['cityZIP'])) {
 	echo '{"success": 1, "message": "hey"}';
 } else {
 	echo '{"success": 0, "message": "hey"}';
