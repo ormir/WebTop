b43c85b02ca28708e0833c684e3366e61afee385
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-05-19 16:18:33 +0200

    client and result

--- ajax/weather_data.php
+++ ajax/weather_data.php
@@ -3,7 +3,8 @@ header('Content-Type: application/json');
 
 if (isset($_POST['cityZIP'])){
 	$client = new SoapClient("http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL");
-	print_r($client);
+	$result=$client->GetCityWeatherByZIP(array("ZIP"=>$_POST['cityZIP']));
+	$result= $result["GetCityWeatherByZIP"];
 } else {
 	echo '{"success": 0, "message": "hey"}';
 }
