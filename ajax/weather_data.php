3c4021514e4cc2351b984d9cf4ee5b180ebcbfc6
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-05-19 16:03:42 +0200

    client soap client

--- ajax/weather_data.php
+++ ajax/weather_data.php
@@ -2,7 +2,8 @@
 header('Content-Type: application/json');
 
 if (isset($_POST['cityZIP'])){
-	echo '{"success": 1, "message": "hey"}';
+	$client = new SoapClient("http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL");
+	print_r($client);
 } else {
 	echo '{"success": 0, "message": "hey"}';
 }
