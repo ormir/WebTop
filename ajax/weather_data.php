6ea30fb8f58204a93a3376c66ff06cac4730fb19
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-05-20 14:24:39 +0200

    weather app done :DDDDDD

--- ajax/weather_data.php
+++ ajax/weather_data.php
@@ -7,14 +7,26 @@ if (isset($_POST['cityZIP'])){
 	$client = new SoapClient("http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL");
 	$result=$client->GetCityWeatherByZIP(array("ZIP"=>$_POST['cityZIP']));
 	$result= $result->GetCityWeatherByZIPResult;
+	$weatherid = $client->GetWeatherInformation(array());
+	$weatherid = $weatherid->GetWeatherInformationResult->WeatherDescription;
+
+	for ($i=0; $i < sizeof($weatherid); $i++) { 
+		// print_r($weatherid[$i]);
+		// print_r($result);
+		// echo $weatherid[1]->WeatherID;
+		if($weatherid[$i]->WeatherID == $result->WeatherID){
+			$response["imgURL"] = $weatherid[$i]->PictureURL;
+		}
+	}
 	$response["success"] = 1;
 	$response["message"] = $result;
 } else {
 	$response["success"] = 0;
 	$response ["message"] = "Missing Zip";
-	// is the same as -> echo '{"success": 0, "message": "hey"}';
+	// is the same as -> echo '{"success": 0, "message": "Missing Zip"}';
 }
 
 echo json_encode($response);
+
 
 ?>
--- js/weather.js
+++ js/weather.js
@@ -12,6 +12,7 @@ $(function(){
 				$('#weather_city').html(response.message.City);
 				$('#weather_desc').html(response.message.Description);
 				$('#weather_temp').html(response.message.Temperature);
+				$('#weathernow').attr("src", response.imgURL);
 
 			}else{
 				console.log(response.message);
